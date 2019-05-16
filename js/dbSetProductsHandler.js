c1fe0e0fdf3c1dffb8467fbd0877c711578242b5
nofar <nofaralfasi@gmail.com>
2019-05-17 00:06:51 +0300

    firebase cloud firestore sent id of a product to app.js when clicked - working! added wallpapers json from csv to cloud firestore

--- js/dbSetProductsHandler.js
+++ js/dbSetProductsHandler.js
@@ -14,7 +14,7 @@ const collection = firebase.firestore().collection("products");
 
 collection.doc("1905-18-53-1").set({
     "name": "'Brick wallpaper in gray and vintage white",
-    "collection": "Home Wallpapers",
+    "collection": ["Home Wallpapers"],
     "sku": "1905-18-53-1",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -30,7 +30,7 @@ collection.doc("1905-18-53-1").set({
 
 collection.doc("1905-15-68-1").set({
     "name": "A prestigious and modern geometric wallpaper",
-    "collection": "Home Wallpapers",
+    "collection": ["Home Wallpapers"],
     "sku": "1905-15-68-1",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -40,7 +40,7 @@ collection.doc("1905-15-68-1").set({
 
 collection.doc("1903-18-84").set({
     "name": "White Brick wallpaper",
-    "collection": "Home Wallpapers",
+    "collection": ["Home Wallpapers"],
     "sku": "1903-18-84",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -50,7 +50,7 @@ collection.doc("1903-18-84").set({
 
 collection.doc("1905-32-89-1").set({
     "name": "Geometric black and white triangles",
-    "collection": "Home Wallpapers;Kids Wallpapers",
+    "collection": ["Home Wallpapers, Kids Wallpapers"],
     "sku": "1905-32-89-1",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -58,9 +58,9 @@ collection.doc("1905-32-89-1").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0001").set({
     "name": "Wallpaper like marble tiles for a bedroom and a living room",
-    "collection": "Home Wallpapers",
+    "collection": ["Home Wallpapers"],
     "sku": "",
     "price": 400,
     "discountMode": "AMOUNT",
@@ -68,9 +68,9 @@ collection.doc(" ").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0002").set({
     "name": "Wallpaper gorgeous and modern stripes",
-    "collection": "Home Wallpapers",
+    "collection": ["Home Wallpapers"],
     "sku": "",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -80,7 +80,7 @@ collection.doc(" ").set({
 
 collection.doc("1905-31-80-1").set({
     "name": "Sky wallpaper clouds and stars",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "1905-31-80-1",
     "price": 425,
     "discountMode": "AMOUNT",
@@ -88,9 +88,9 @@ collection.doc("1905-31-80-1").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0003").set({
     "name": "Luxurious striped wallpaper for a living room",
-    "collection": "Home Wallpapers",
+    "collection": ["Home Wallpapers"],
     "sku": "",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -98,9 +98,9 @@ collection.doc(" ").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0004").set({
     "name": "Wallpaper roll round modern and beautiful",
-    "collection": "Home Wallpapers",
+    "collection": ["Home Wallpapers"],
     "sku": "",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -110,7 +110,7 @@ collection.doc(" ").set({
 
 collection.doc("1905-31-79-1").set({
     "name": "Cartoon wallpaper Puppy for children's rooms",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "1905-31-79-1",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -120,7 +120,7 @@ collection.doc("1905-31-79-1").set({
 
 collection.doc("1905-26-80-1").set({
     "name": "Decorative and warm wallpaper for children's room",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "1905-26-80-1",
     "price": 400,
     "discountMode": "AMOUNT",
@@ -128,9 +128,9 @@ collection.doc("1905-26-80-1").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0005").set({
     "name": "Modern wallpaper strippers",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -140,7 +140,7 @@ collection.doc(" ").set({
 
 collection.doc("1905-18-04-1").set({
     "name": "Step up to a children's room",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "1905-18-04-1",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -148,9 +148,9 @@ collection.doc("1905-18-04-1").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0006").set({
     "name": "Wallpaper geometric triangles",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "",
     "price": 415,
     "discountMode": "AMOUNT",
@@ -158,9 +158,9 @@ collection.doc(" ").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0007").set({
     "name": "High quality wallpaper in a variety of colors",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -168,9 +168,9 @@ collection.doc(" ").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0008").set({
     "name": "A pastoral and romantic flower wallpaper",
-    "collection": "Home Wallpapers;Kids Wallpapers",
+    "collection": ["Home Wallpapers, Kids Wallpapers"],
     "sku": "",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -180,7 +180,7 @@ collection.doc(" ").set({
 
 collection.doc("1905-46-79-1").set({
     "name": "Wallpaper American Style Stars",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "1905-46-79-1",
     "price": 400,
     "discountMode": "AMOUNT",
@@ -188,9 +188,9 @@ collection.doc("1905-46-79-1").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0009").set({
     "name": "Wallpaper of a sailboat",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "",
     "price": 500,
     "discountMode": "AMOUNT",
@@ -198,9 +198,9 @@ collection.doc(" ").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0010").set({
     "name": "Bear wallpaper for children's room",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -208,9 +208,9 @@ collection.doc(" ").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0011").set({
     "name": "Wallpaper of a painted cat",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -218,9 +218,9 @@ collection.doc(" ").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0012").set({
     "name": "Climb the stripes",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -228,9 +228,9 @@ collection.doc(" ").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0013").set({
     "name": "Wallpaper thick hearts especially",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "",
     "price": 419,
     "discountMode": "AMOUNT",
@@ -238,9 +238,9 @@ collection.doc(" ").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0014").set({
     "name": "Princess wallpaper for children's rooms",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "",
     "price": 419,
     "discountMode": "AMOUNT",
@@ -248,9 +248,9 @@ collection.doc(" ").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0015").set({
     "name": "Wallpaper of a hot air balloon",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -258,9 +258,9 @@ collection.doc(" ").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0016").set({
     "name": "Desktop wallpaper",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -270,7 +270,7 @@ collection.doc(" ").set({
 
 collection.doc("1905-20-51-1").set({
     "name": "Hello Kitty Cat Wallpaper",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "1905-20-51-1",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -278,9 +278,9 @@ collection.doc("1905-20-51-1").set({
 });
 
 
-collection.doc(" ").set({
+collection.doc("0017").set({
     "name": "Wallpaper A ballet dancer for a childhood room",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "",
     "price": 385,
     "discountMode": "AMOUNT",
@@ -290,7 +290,7 @@ collection.doc(" ").set({
 
 collection.doc("1903-18-89").set({
     "name": "Two-tone vertical striped wallpaper for a children's room",
-    "collection": "Kids Wallpapers",
+    "collection": ["Kids Wallpapers"],
     "sku": "1903-18-89",
     "price": 385,
     "discountMode": "AMOUNT",
