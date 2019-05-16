c5cf6a7acb99300e5278edf0f4e9d0a0e5b48219
nofar <nofaralfasi@gmail.com>
2019-05-16 10:09:38 +0300

    firebase cloud firestore

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
 // initialize firebase
-const firebaseConfig = {
+firebase.initializeApp({
     apiKey: "AIzaSyBrc5-k7DLBqBb_Sikwr7gjYuym9DTzePA",
     authDomain: "stickers-center-2019.firebaseapp.com",
     databaseURL: "https://stickers-center-2019.firebaseio.com",
@@ -7,10 +11,11 @@ const firebaseConfig = {
     storageBucket: "stickers-center-2019.appspot.com",
     messagingSenderId: "561200702492",
     appId: "1:561200702492:web:ab68b68c290064a6"
-};
-firebase.initializeApp(firebaseConfig);
+});
+// firebase.initializeApp(firebaseConfig);
+var firestore = firebase.firestore();
 
-const docRef = firestore().doc("users/2");
+const docRef = firestore.doc("users/2");
 const outputHeader = document.querySelector("#stayUpdated");
 const inputTextField = document.querySelector("#userEmail");
 const submitButton = document.querySelector("#submitEmail");
