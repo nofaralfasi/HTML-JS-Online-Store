3ed77934cc3242196754f1d28c72ef55acbb7dc4
nofar <nofaralfasi@gmail.com>
2019-05-18 11:05:39 +0300

    starting to use firestore storage and fully deployment of the website on firebase moved folders to public folder in order to fully display website's content added init-firebase.js file

---
+++ public/js/init-firebase.js
@@ -0,0 +1,13 @@
+// Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBrc5-k7DLBqBb_Sikwr7gjYuym9DTzePA",
+    authDomain: "stickers-center-2019.firebaseapp.com",
+    databaseURL: "https://stickers-center-2019.firebaseio.com",
+    projectId: "stickers-center-2019",
+    storageBucket: "stickers-center-2019.appspot.com",
+    messagingSenderId: "561200702492",
+    appId: "1:561200702492:web:ab68b68c290064a6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
