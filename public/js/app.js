fbbaec82ffbc7d13730990a6d0dc12083b0a1063
nofar <nofaralfasi@gmail.com>
2019-05-28 12:09:47 +0300

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,10 @@ const firebaseConfig = {
     authDomain: "stickers-center-2019.firebaseapp.com",
     databaseURL: "https://stickers-center-2019.firebaseio.com",
     projectId: "stickers-center-2019",
-    storageBucket: "stickers-center-2019.appspot.com",
-    messagingSenderId: "561200702492",
-    appId: "1:561200702492:web:ab68b68c290064a6"
+    storageBucket: "stickers-center-2019.appspot.com" //,
+    // deleted for security reasons
+    // messagingSenderId: "",
+    // appId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
